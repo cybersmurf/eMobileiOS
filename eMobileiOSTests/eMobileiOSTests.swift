ed287ccde96a16da9ed255210b9405a7cf471be9
sud <petr@petrsramek.eu>
2024-10-10 19:58:56 +0200

    Initial Commit

---
+++ eMobileiOSTests/eMobileiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  eMobileiOSTests.swift
+//  eMobileiOSTests
+//
+//  Created by Petr Sramek on 10.10.2024.
+//
+
+import Testing
+@testable import eMobileiOS
+
+struct eMobileiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
